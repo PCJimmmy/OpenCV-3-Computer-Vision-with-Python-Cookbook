08be606384e3439183599c147291901d80fc8310
PCJimmmy <pcjimmmy@gmail.com>
2019-05-17 22:45:55 -0400

    Update 01 Reading image from file.py

--- Chapter01/01 Reading image from file.py
+++ Chapter01/01 Reading image from file.py
@@ -6,7 +6,7 @@ import argparse
 import cv2
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', default='../data/Lena.png', help='Image path.')
+parser.add_argument('--path', default='d:/opencv/Lena.png', help='Image path.')
 params = parser.parse_args()
 
 img = cv2.imread(params.path)
